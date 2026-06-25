29ee8b35df5c98b6aa58a8d106cf0789949fe0d7
Forget-Dust <858145049@qq.com>
2026-06-25 08:20:09 +0800

    2026.6.25

---
+++ .github/Install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# for f in */*.sh;do f="${f}";rm -rf "${f}";done
+# for d in */;do d="${d%/}";cp -rf Install.sh ${d};done
+# for f in *.run;do f="${f}";./${f} --target ${f%.run};done
+# for d in */;do d="${d%/}";makeself --xz "${d}" "${d}.run" "${d}" ./Install.sh;done
+
+if find . -type f -name '*.run' | grep -q "/";then
+    find . -type f -name '*.run' -exec sh -c 'chmod +x "$1" && sh "$1" && rm -rf "$1"' _ {} \;
+else
+    if command -v opkg >/dev/null 2>&1;then
+        echo "==> Update List..." && opkg update && clear
+
+        echo "==> Installing packages..."
+        find . -type f -name "*$(uname -m)*.ipk" -exec sh -c 'opkg install "$1" 2>/dev/nul && rm -f "$1"' _ {} \;
+        find . -type f -name "luci-*.ipk" -exec sh -c 'opkg install "$1" 2>/dev/nul && rm -f "$1"' _ {} \;
+        find . -type f -name "*.ipk" -exec sh -c 'opkg install "$1" 2>/dev/nul && rm -f "$1"' _ {} \;
+
+        echo "==> Done" ;elif command -v apk >/dev/null 2>&1;then
+        echo "==> Update List..." && apk update && clear
+
+        echo "==> Installing packages..."
+        find . -type f -name "*$(uname -m)*.ipk" -exec sh -c 'apk add --force-overwrite "$1" 2>/dev/nul && rm -f "$1"' _ {} \;
+        find . -type f -name "luci-*.ipk" -exec sh -c 'apk add --force-overwrite "$1" 2>/dev/nul && rm -f "$1"' _ {} \;
+        find . -type f -name "*.ipk" -exec sh -c 'apk add --force-overwrite "$1" 2>/dev/nul && rm -f "$1"' _ {} \;
+
+        echo "==> Done";else echo "==> System Not Supported";fi
+fi
---
+++ README.md
@@ -0,0 +1,16 @@
+## OpenWrt 插件
+| 名称 | 上游 |
+|----|----|
+| AdGuardHome | https://github.com/rufengsuixing/luci-app-adguardhome |
+| Amlogic | https://github.com/ophub/luci-app-amlogic |
+| Argon | https://github.com/jerrykuku/luci-theme-argon |
+| DiskMan | https://github.com/lisaac/luci-app-diskman |
+| Lucky | https://github.com/gdy666/luci-app-lucky |
+| OpenList2 | https://github.com/sbwml/luci-app-openlist2 |
+| SmartDNS | https://github.com/pymumu/smartdns |
+| OpenClash | https://github.com/vernesong/OpenClash |
+| Istore | https://istore.istoreos.com/repo/all |
+| Others | [OpenWrt 24](https://downloads.openwrt.org/releases/24.10.7/packages/)  [OpenWrt 25](https://downloads.openwrt.org/releases/25.12.4/packages/)|
+
+## OpenWrt 镜像
+> 基于官方包构建：界面汉化、添加（ipv6并设置自动获取,istore商店,磁盘管理,挂载点,Argon主题）
